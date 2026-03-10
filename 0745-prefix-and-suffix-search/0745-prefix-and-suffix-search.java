4ce32dc3ba3e25d7374b69c1e648c1ee7b66f7dc
Ayush Jain <ayushjain67429@gmail.com>
2026-03-10 20:08:38 +0530

    Time: 842 ms (5.44%), Space: 273.6 MB (45.7%) - LeetHub

---
+++ 0745-prefix-and-suffix-search/0745-prefix-and-suffix-search.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class WordFilter {
+
+    private Map<String, Integer> map;
+
+    public WordFilter(String[] words) {
+        map = new HashMap<>();
+
+        for (int index = 0; index < words.length; index++) {
+            String word = words[index];
+            int len = word.length();
+
+            for (int i = 0; i <= len; i++) {
+                String prefix = word.substring(0, i);
+                for (int j = 0; j <= len; j++) {
+                    String suffix = word.substring(len - j);
+                    map.put(prefix + "#" + suffix, index);
+                }
+            }
+        }
+    }
+
+    public int f(String pref, String suff) {
+        return map.getOrDefault(pref + "#" + suff, -1);
+    }
+}
