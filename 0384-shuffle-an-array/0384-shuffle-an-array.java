6f4be9bc479720561d84760a968b712adf5f08bf
Ayush Jain <ayushjain67429@gmail.com>
2026-03-10 16:17:46 +0530

    Time: 52 ms (54.06%), Space: 52.5 MB (40.17%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    private int[] original;
+    private int[] array;
+    private Random rand;
+
+    public Solution(int[] nums) {
+        original = nums.clone();
+        array = nums.clone();
+        rand = new Random();
+    }
+    
+    public int[] reset() {
+        array = original.clone();
+        return array;
+    }
+    
+    public int[] shuffle() {
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1);
+            
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
