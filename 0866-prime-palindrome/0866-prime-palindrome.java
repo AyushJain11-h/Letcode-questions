8cd11fa55c2568775782377db1cbf59b480d9401
Ayush Jain <ayushjain67429@gmail.com>
2026-03-26 15:45:10 +0530

    Time: 50 ms (31.86%), Space: 46.3 MB (35.29%) - LeetHub

---
+++ 0866-prime-palindrome/0866-prime-palindrome.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int primePalindrome(int n) {
+        while (true) {
+            if (isPalindrome(n) && isPrime(n)) {
+                return n;
+            }
+            n++;
+            
+            // All even length palindromes > 11 are divisible by 11
+            if (10000000 < n && n < 100000000) {
+                n = 100000000;
+            }
+        }
+    }
+
+    private boolean isPalindrome(int n) {
+        String s = Integer.toString(n);
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPrime(int n) {
+        if (n < 2) return false;
+        if (n % 2 == 0) return n == 2;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
