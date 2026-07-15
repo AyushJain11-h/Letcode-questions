a710787532379154a1c65fdc3ffc9180f1616ea7
Ayush Jain <ayushjain67429@gmail.com>
2026-07-15 17:59:55 +0530

    Time: 40 ms (97.79%), Space: 64.8 MB (10.73%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,27 +1,22 @@
 class Solution {
     public int reversePairs(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         return mergeSort(nums, 0, nums.length - 1);
     }
 
     private int mergeSort(int[] nums, int left, int right) {
-        if (left >= right) return 0;
+        if (left >= right) {
+            return 0;
+        }
 
         int mid = left + (right - left) / 2;
-        int count = 0;
+        int count = mergeSort(nums, left, mid)
+                  + mergeSort(nums, mid + 1, right);
 
-        count += mergeSort(nums, left, mid);
-        count += mergeSort(nums, mid + 1, right);
-        count += countPairs(nums, left, mid, right);
-        merge(nums, left, mid, right);
-
-        return count;
-    }
-
-    private int countPairs(int[] nums, int left, int mid, int right) {
-        int count = 0;
+        // Count reverse pairs
         int j = mid + 1;
-
         for (int i = left; i <= mid; i++) {
             while (j <= right && (long) nums[i] > 2L * nums[j]) {
                 j++;
@@ -29,12 +24,10 @@ class Solution {
             count += j - (mid + 1);
         }
 
-        return count;
-    }
-
-    private void merge(int[] nums, int left, int mid, int right) {
+        // Merge two sorted halves
         int[] temp = new int[right - left + 1];
-        int i = left, j = mid + 1, k = 0;
+        int i = left, k = 0;
+        j = mid + 1;
 
         while (i <= mid && j <= right) {
             if (nums[i] <= nums[j]) {
@@ -52,8 +45,8 @@ class Solution {
             temp[k++] = nums[j++];
         }
 
-        for (int p = 0; p < temp.length; p++) {
-            nums[left + p] = temp[p];
-        }
+        System.arraycopy(temp, 0, nums, left, temp.length);
+
+        return count;
     }
 }
