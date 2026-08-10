c931300a8dc889f48946e375335b6721d145187c
Ayush Jain <ayushjain67429@gmail.com>
2026-08-10 12:19:10 +0530

    Time: 22 ms (67.79%), Space: 66.5 MB (71.16%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -2,28 +2,40 @@ import java.util.*;
 
 class Solution {
     public int shortestSubarray(int[] nums, int k) {
+
         int n = nums.length;
+
+        // Prefix sum
         long[] prefix = new long[n + 1];
 
         for (int i = 0; i < n; i++) {
             prefix[i + 1] = prefix[i] + nums[i];
         }
 
+        // Monotonic increasing deque of indices
         Deque<Integer> deque = new ArrayDeque<>();
-        int minLength = n + 1;
+
+        int ans = n + 1;
 
         for (int i = 0; i <= n; i++) {
-            while (!deque.isEmpty() && prefix[i] - prefix[deque.peekFirst()] >= k) {
-                minLength = Math.min(minLength, i - deque.pollFirst());
+
+            // Check if current prefix sum - oldest prefix sum >= k
+            while (!deque.isEmpty()
+                    && prefix[i] - prefix[deque.peekFirst()] >= k) {
+
+                ans = Math.min(ans, i - deque.pollFirst());
             }
 
-            while (!deque.isEmpty() && prefix[i] <= prefix[deque.peekLast()]) {
+            // Maintain increasing prefix sums
+            while (!deque.isEmpty()
+                    && prefix[i] <= prefix[deque.peekLast()]) {
+
                 deque.pollLast();
             }
 
             deque.offerLast(i);
         }
 
-        return minLength == n + 1 ? -1 : minLength;
+        return ans == n + 1 ? -1 : ans;
     }
 }
