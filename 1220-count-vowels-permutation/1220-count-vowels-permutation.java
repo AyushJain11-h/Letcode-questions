2f919a6606457abd77e93e85518f6d57a868c13b
Ayush Jain <ayushjain67429@gmail.com>
2026-08-13 18:09:24 +0530

    Time: 23 ms (38.42%), Space: 47.1 MB (37.54%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,23 +1,41 @@
 class Solution {
     public int countVowelPermutation(int n) {
-        long MOD = 1_000_000_007;
-
-        long a = 1, e = 1, i = 1, o = 1, u = 1;
-
-        for (int step = 2; step <= n; step++) {
-            long na = (e + i + u) % MOD;
-            long ne = (a + i) % MOD;
-            long ni = (e + o) % MOD;
-            long no = i % MOD;
-            long nu = (i + o) % MOD;
-
-            a = na;
-            e = ne;
-            i = ni;
-            o = no;
-            u = nu;
+        final int MOD = 1000000007;
+        
+        // dp[i][j] = number of strings of length i ending with vowel j
+        // 0:a, 1:e, 2:i, 3:o, 4:u
+        long[][] dp = new long[n + 1][5];
+        
+        // Base case: strings of length 1
+        for (int j = 0; j < 5; j++) {
+            dp[1][j] = 1;
         }
-
-        return (int)((a + e + i + o + u) % MOD);
+        
+        // Fill dp table for lengths 2 to n
+        for (int i = 2; i <= n; i++) {
+            // Each vowel can be followed by certain vowels
+            // 'a' can be followed by: 'e' only
+            dp[i][0] = dp[i-1][1] % MOD;
+            
+            // 'e' can be followed by: 'a', 'i'
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
+            
+            // 'i' can be followed by: 'a', 'e', 'o', 'u'
+            dp[i][2] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][3] + dp[i-1][4]) % MOD;
+            
+            // 'o' can be followed by: 'i', 'u'
+            dp[i][3] = (dp[i-1][2] + dp[i-1][4]) % MOD;
+            
+            // 'u' can be followed by: 'a' only
+            dp[i][4] = dp[i-1][0] % MOD;
+        }
+        
+        // Sum all strings of length n ending with each vowel
+        long result = 0;
+        for (int j = 0; j < 5; j++) {
+            result = (result + dp[n][j]) % MOD;
+        }
+        
+        return (int) result;
     }
 }
